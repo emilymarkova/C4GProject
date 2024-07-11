38d1bf1e2682ef6caf701d52635f5e3eb948e4a5
bigalex2k <bigalex20007@gmail.com>
2024-07-10 21:23:12 -0600

    I did stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import '../App.css';
-import Header from '../Components/Header';
 import Background from '../Components/Background';
 import PageContent from '../Components/PageContent';
 import NavBar from '../Components/NavBar';
@@ -15,30 +13,8 @@ export default function Home() {
     @import url('https://fonts.googleapis.com/css2?family=Gaegu&display=swap');
     </style>
     <Background/>
-      {/* <Header/> */}
       <NavBar/>
     <PageContent/>
     </>
-  );
-=======
-import React from 'react';
-import '../App.css';
-import Header from '../Components/Header';
-import Background from '../Components/Background';
-import PageContent from '../Components/PageContent';
-
-
-
-export default function Home() {
-  return ( 
-    <>
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Gaegu&display=swap');
-    </style>
-    <Background/>
-    <Header/>
-    <PageContent/>
-    </>
-  );
->>>>>>> Stashed changes
+  )
 }
