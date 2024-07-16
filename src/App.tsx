8e01a633a33fb1eee1e3df2c44cd1da7e86a775a
emilym <emily.markova@gmail.com>
2024-07-16 14:49:34 -0700

    Update nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ function App() {
         <Route path="/login"     element={<Login />}></Route>
         <Route path="/account" element={<Account />}></Route>
         <Route path="/calendar" element={<FullPageCalendar />}></Route>
-        <Route path="signUp"     element={<SignUp />}></Route>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/signUp"     element={<SignUp />}></Route>
         <Route path="/about"     element={<About />}></Route>
         <Route path="/notes"     element={<Notes />}></Route>
       </Routes>
